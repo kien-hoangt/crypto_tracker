aa3509e145622b105164dceeef59469893d55b7c
kienht99 <htkien.dev@gmail.com>
2022-05-08 15:10:12 +0700

    - Feat: Add search bar [Home]

---
+++ CryptoTracker/Core/Components/SearchBar/SearchBarView.swift
@@ -0,0 +1,58 @@
+//
+//  SearchBarView.swift
+//  CryptoTracker
+//
+//  Created by Bradley Hoang on 08/05/2022.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    @Binding var searchText: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(
+                    searchText.isEmpty ?
+                    Color.theme.secondaryText : Color.theme.accent
+                )
+            
+            TextField("Search by name or symbol...", text: $searchText)
+                .foregroundColor(Color.theme.accent)
+                .disableAutocorrection(true)
+                .overlay(
+                    Image(systemName: "xmark.circle.fill")
+                        .padding()
+                        .offset(x: 10)
+                        .foregroundColor(Color.theme.accent)
+                        .opacity(searchText.isEmpty ? 0 : 1)
+                        .onTapGesture {
+                            searchText = ""
+                            UIApplication.shared.endEditing()
+                        },
+                    alignment: .trailing
+                )
+        }
+        .font(.headline)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.theme.background)
+                .shadow(color: Color.theme.accent.opacity(0.15), radius: 10, x: 0, y: 0)
+        )
+        .padding()
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            SearchBarView(searchText: .constant(""))
+                .previewLayout(.sizeThatFits)
+            SearchBarView(searchText: .constant(""))
+                .preferredColorScheme(.dark)
+                .previewLayout(.sizeThatFits)
+        }
+    }
+}
--- CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -11,6 +11,7 @@ import Combine
 class HomeViewModel: ObservableObject {
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
+    @Published var searchText: String = ""
     
     private let dataService = CoinDataService()
     private var cancellables = Set<AnyCancellable>()
@@ -20,10 +21,27 @@ class HomeViewModel: ObservableObject {
     }
     
     private func addSubscribers() {
-        dataService.$allCoins
+        // updates allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        return coins.filter { coin -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
