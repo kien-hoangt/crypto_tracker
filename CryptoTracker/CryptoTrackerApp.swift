dcfbd1239607104f3f8fc4579dad3bb706bf5b74
kienht99 <htkien.dev@gmail.com>
2022-06-02 23:03:48 +0700

    - Feat: Add launch screen

--- CryptoTracker/CryptoTrackerApp.swift
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @main
 struct CryptoTrackerApp: App {
     @StateObject private var vm = HomeViewModel()
+    @State private var showLaunchView: Bool = true
     
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
@@ -20,11 +21,21 @@ struct CryptoTrackerApp: App {
     
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                HomeView()
-                    .navigationBarHidden(true)
+            ZStack {
+                NavigationView {
+                    HomeView()
+                        .navigationBarHidden(true)
+                }
+                .environmentObject(vm)
+                
+                ZStack {
+                    if showLaunchView {
+                        LaunchView(showLaunchView: $showLaunchView)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+                .zIndex(2.0)
             }
-            .environmentObject(vm)
         }
     }
 }
